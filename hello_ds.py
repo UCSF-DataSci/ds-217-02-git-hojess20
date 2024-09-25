28a7a8bf3b408b3632ae5753c4df885921963901
hojess20 <60207354+hojess20@users.noreply.github.com>
2024-09-25 07:42:03 -0700

    Add screenshot and meme to readme.md

--- hello_ds.py
+++ hello_ds.py
@@ -1,1 +1,11 @@
 print("Hello, Data Science!")
+
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get (3, 5, 6, 9). 
+# The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum_multiples = 0
+
+for x in range(1000):
+    if x % 3 == 0 or x % 5 == 0:
+        sum_multiples += x
+print(sum_multiples)
